67b084e3a6b8b40c6860f6681893d8bdb221f1dc
sowmya97github <sowmyacm97@gmail.com>
2024-07-21 13:09:37 +0530

    2nd push

--- src/test/java/demo/script2Test.java
+++ src/test/java/demo/script2Test.java
@@ -3,7 +3,8 @@ package demo;
 import org.testng.annotations.Test;
 
 public class script2Test {
-	//Hello
+	//Hello 
+	//Hey Java Selenium
 	@Test
 	public void methodTest(){
 		System.out.println("script2Test pass");
