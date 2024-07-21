d0b019fe4daae63e93ecb60e0abf4bd1701a5949
sowmya97github <sowmyacm97@gmail.com>
2024-07-21 12:57:25 +0530

    push 1

---
+++ src/test/java/demo/script2Test.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class script2Test {
+	
+	@Test
+	public void methodTest(){
+		System.out.println("script2Test pass");
+	}
+
+}
---
+++ src/test/java/demo/scriptTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class scriptTest {
+	
+	@Test
+	public void methodTest(){
+		System.out.println("scriptTest pass");
+	}
+
+}
